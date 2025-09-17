d5510f2eac40728c3e8d8694f279f094626961b1
Wolf Mermelstein <wolf@404wolf.com>
2025-09-17 12:15:11 -0400

    Change to nixos unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "CSDS 377 Compiler";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix/monthly";
